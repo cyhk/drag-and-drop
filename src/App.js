a8d4b86dc48e4a8b4daeb3be0a4ca623b30b1f48
cyhk <cyhk116@gmail.com>
2019-10-06 00:58:56 -0700

    built column and task components and started integrating react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './Column';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [ data, setData ] = useState(initialData);
+
+  const columns = data.columnOrder.map(columnId => {
+    const column = data.columns[columnId];
+    const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+
+    return <Column key={column.id} column={column} tasks={tasks} />;
+  });
+
+  const onDragEnd = result => {
+    
+  };
+
+  return <DragDropContext
+    onDragEnd={onDragEnd}
+  >
+    {columns}
+  </DragDropContext>
 }
 
 export default App;
---
+++ src/initial-data.js
@@ -0,0 +1,15 @@
+const initialData = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'Take out the garbage'},
+    'task-2': { id: 'task-2', content: 'Make dinner'},
+    'task-3': { id: 'task-3', content: 'Watch a film' }
+  }, columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2', 'task-3']
+    }
+  }, columnOrder: ['column-1'],
+}
+
+export default initialData;
